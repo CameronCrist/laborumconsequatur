9b14c0e4621985595665892168a431d856d70f0d
CameronCrist <dannyweiss3a947a659031@outlook.com>
2024-03-22 02:40:24 +0800

    maxime

---
+++ index.js
@@ -0,0 +1,59 @@
+/*!
+ * js-comments <https://github.com/jonschlinkert/js-comments>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors
+ * Licensed under the MIT License (MIT)
+ */
+
+'use strict';
+
+var path = require('path');
+var file = require('fs-utils');
+var comments = require('./lib');
+var _ = require('lodash');
+
+// Default readme template
+var defaultTemplate = require('js-comments-template').readme;
+
+/**
+ * ```js
+ * var comments = require('js-comments');
+ * var docs = comments(string);
+ * ```
+ * See [example output](./test/actual/comments.json).
+ * See [example code comments](./index.js).
+ *
+ * @param   {String} `str` The source string.
+ * @param   {String} `dest` Optional destination file path for generating relative links.
+ * @param   {Object} `options`
+ * @return  {String} Rendered documentation.
+ */
+
+module.exports = function (src, dest, options) {
+  if (typeof src === 'object') {
+    options = src;
+    dest = options.dest;
+    src = options.src;
+  }
+  if (typeof dest === 'object') {
+    options = dest;
+    dest = process.cwd();
+  }
+
+  options = options || {};
+  dest = options.dest || process.cwd();
+
+  // The lodash template to use for comments
+  var template = options.template || defaultTemplate;
+  var json = comments(src, dest, options);
+  if (options.json) {
+    file.writeJSONSync(options.json, json);
+  }
+
+  var docs = _.template(template, {
+    files: json
+  });
+
+  // Remove leading and trailing whitespace
+  return docs.replace(/^\s+|\s+$/g, '');
+};
---
+++ test/parse.js
@@ -0,0 +1,74 @@
+/*!
+ * js-comments <https://githuc.com/jonschlinkert/js-comments>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors
+ * Licensed under the MIT License (MIT)
+ */
+
+var file = require('fs-utils');
+var expect = require('chai').expect;
+var parseComment = require('../lib/index');
+
+function readFixture(src) {
+  var str = file.readFileSync('test/fixtures/' + src + '.js');
+  return parseComment(str);
+}
+
+var comment = [
+  '/**',
+  ' * ## .parser',
+  ' *',
+  ' * Set a parser that can later be used to parse any given string.',
+  ' *',
+  ' * ```js',
+  ' * strings.parser (name, replacements)',
+  ' * ```',
+  ' *',
+  ' * **Example**',
+  ' *',
+  ' * {%= docs("example-parser.md") %}',
+  ' *',
+  ' * @param {String} `name`',
+  ' * @param {Object|Array} `arr` Object or array of replacement patterns to associate.',
+  ' *   @property {String|RegExp} `pattern`',
+  ' *   @property {String|Function} `replacement`',
+  ' * @return {Strings} to allow chaining',
+  ' *   @property {Array} `foo`',
+  ' * @api public',
+  ' */'
+].join('\n');
+
+describe('when a string is passed:', function () {
+  it('should parse a string', function () {
+    var actual = parseComment('/**\n@foo {Object} `bar`\n*/')
+    console.log(actual)
+    // actual.comments.length.should.be(1);
+    // actual.comments[0].should.have.property('foo');
+  });
+
+  // it('should parse a string', function () {
+  //   var actual = parseComment(comment)
+  //   actual.comments.length.should.be(1);
+  //   actual.comments[0].should.have.property('param');
+  // });
+
+  // it('should parse @params', function () {
+  //   var actual = readFixture('params');
+  //   actual.comments.length.should.be(1);
+  //   actual.comments[0].should.have.property('param');
+  // });
+
+  // it('should parse @return', function () {
+  //   var actual = readFixture('return');
+  //   actual.comments.length.should.be(1);
+  //   actual.comments[0].should.have.property('return');
+  // });
+
+  // it('should parse @api', function () {
+  //   var actual = readFixture('api');
+  //   actual.comments.length.should.be(1);
+  //   actual.comments[0].should.have.property('api');
+  // });
+
+});
+
